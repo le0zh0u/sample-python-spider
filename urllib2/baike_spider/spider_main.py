3793cb45c4191d74a01e944d19f5d921272777bd
leozhou <492753662@qq.com>
2016-02-13 01:48:58 +0800

    add files

---
+++ urllib2/baike_spider/spider_main.py
@@ -0,0 +1,8 @@
+class SpiderMain(object):
+    pass
+
+
+if __name__ == "__main__":
+    root_url = "http://baike.baidu.com/view/21087.htm"
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)
